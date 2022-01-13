c578e95f44e896018cfcbe0233a86054c272ed24
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-13 10:47:57 -0500

    initial upload of mapTest.js

---
+++ d4-funcs/map_builtin/mapTest.js
@@ -0,0 +1,4 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+console.log(lighthouses.map(e => e.length))
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
