6b9897fffbc10e3a1cd0a56df6dfbb055f44011c
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-13 11:09:25 -0500

    updating for eslint compatability

--- d4-funcs/map_builtin/mapTest.js
+++ d4-funcs/map_builtin/mapTest.js
@@ -1,4 +1,12 @@
-const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+const lighthouses = [
+  'Gibraltar Point',
+  "Peggy's Point",
+  'Cove Island',
+  'Discovery Island',
+  'Cape Scott',
+  'Point Clark',
+  'Kincardine'
+];
 
-console.log(lighthouses.map(e => e.length))
+console.log(lighthouses.map(e => e.length));
 // It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
